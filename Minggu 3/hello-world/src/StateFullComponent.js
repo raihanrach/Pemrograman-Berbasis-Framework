87a60cf2231411ca54e649046d661fc3e8d06dfe
raihanrach <raihanrach@gmail.com>
2021-02-23 14:37:19 +0700

    update

---
+++ Minggu 3/hello-world/src/StateFullComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const HelloComponent( )=>{
+    return HelloComponent}
+
+class StateFullComponent extends React.components{
+    render(){
+        return<p>StateFullComponent</p>
+    }
+}
+
+ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
+serviceWorker.unregister();
---
+++ Minggu 3/hello-world/src/component/HelloComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HelloComponent(){
+    return <p>HelloComponent</p>
+}
+
+export default HelloComponent;
