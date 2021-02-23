07727671d250f2255d0646de1c5dd652dab73a35
raihanrach <raihanrach@gmail.com>
2021-02-23 09:20:23 +0700

    Tugas Minggu 2

--- Minggu 2/web-react/like_button.js
+++ Minggu 2/web-react/like_button.js
@@ -1,16 +1,13 @@
-// ... the starter code you pasted ...
 const e = React.createElement;
 
 function LikeButton(){
-    // DISPLAY A "Like" <button>
-
     return e(
         'button',
         {
             onClick: () => alert('berhasil')
         },
         'Like'
-        );
+    );
 }
 
 const domContainer = document.querySelector('#like_button_container');
