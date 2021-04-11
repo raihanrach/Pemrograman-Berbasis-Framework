e333147484fcefaff6570fc0cd2177771e31e7f8
raihanrach <raihanrach@gmail.com>
2021-04-11 11:44:12 +0700

    Update Minggu 9

--- Minggu 7/Hoc/react-hoc/src/index.js
+++ Minggu 7/Hoc/react-hoc/src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { reducer as formReducer } from "redux-form";
 import "./index.css";
 import App from "./App";
-import CustomFormDemo from "./CustomForm/CustomFormDemo";
-import GenericContainerDemo from "./GenericContainer/GenericContainerDemo";
-import LoadDemo from "./LoadDemo/LoaderDemo";
-import RequireAuthDemo from "./ProtectedRoutes/RequireAuthDemo";
-import RefsDemo from "./RefsDemo/RefsDemo";
-import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<LoadDemo />, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// import registerServiceWorker from "./registerServiceWorker";
+const rootReducer = combineReducers({
+  form: formReducer,
+});
+const store = createStore(rootReducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+// registerServiceWorker();
 reportWebVitals();
