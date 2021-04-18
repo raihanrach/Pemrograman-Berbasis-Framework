95339f3a8299d4703a11d0f59d485aac7527e12d
raihanrach <raihanrach@gmail.com>
2021-04-18 19:42:57 +0700

    push Minggu 10

---
+++ Minggu 10/minggu10/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+     apiKey: "AIzaSyC-rAruSCFXUOqf_cpTFFi3X6rXEaUQ-qU",
+     authDomain: "minggu10-e383e.firebaseapp.com",
+     projectId: "minggu10-e383e",
+     storageBucket: "minggu10-e383e.appspot.com",
+     messagingSenderId: "411840770419",
+     appId: "1:411840770419:web:9bbbd2296610b6cf4a5506",
+     measurementId: "G-81ZNKLJ31C"
+};
+
