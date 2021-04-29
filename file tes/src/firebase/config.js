c460d6bcfe85091e4c7d875f4e540d74dc683a9e
raihanrach <raihanrach@gmail.com>
2021-04-30 01:00:40 +0700

    push

---
+++ file tes/src/firebase/config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3s5O7KE1kgMqcPUD0-P2vy0e-E4sPypY",
+    authDomain: "minggu11.firebaseapp.com",
+    projectId: "minggu11",
+    databaseURL: "https://minggu11-default-rtdb.firebaseio.com/",
+    storageBucket: "minggu11.appspot.com",
+    messagingSenderId: "927480011216",
+    appId: "1:927480011216:web:62abc67c34150829a001d5"
+};
+
+export default firebaseConfig;
